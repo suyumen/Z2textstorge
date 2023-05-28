5a6622e853fea4aba666c3603a1b958eb08dfc78
suyumen <suyumen@bupt.edu.cn>
2023-05-28 22:41:28 +0800

    url

--- mapper.py
+++ mapper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding:utf-8
-
+import csv
 import sys
 import jieba
 
@@ -9,18 +9,20 @@ def read_file(file_path):
     """读取文件内容并将文件内容标准化处理"""
     with open(file_path, 'r', encoding='utf-8') as f:
         text = f.read()
+        url = text.split('\n')[0]
+        # print(url)
         text = text.strip()  # 去首尾空格
         text = text.lower()  # 转换为小写
-        text = ''.join(filter(lambda x: x >= u'\u4e00' and x <=
-                       u'\u9fa5', text))  # 去除非中文字符
-        return text
+        text = ''.join(filter(lambda x: x >= u'\u4e00' and x <= u'\u9fa5', text))  # 去除非中文字符
+        # print(text)
+        return text, url
 
 
 def mapper():
     for line in sys.stdin:
         file_path = line.strip()
-        file_name = file_path.split('/')[-1]  # 获取文件名
-        text = read_file(file_path)  # 读取文件内容并进行标准化处理
+        file_name = file_path.split('/')[-1].split('.')[0]  # 获取文件名
+        text, url = read_file(file_path)  # 读取文件内容并进行标准化处理
         stopwords = [line.strip() for line in open(
             'new_stopwords.txt', 'r', encoding='utf-8').readlines()]
         # 对文本进行分词，得到词汇列表
@@ -32,8 +34,9 @@ def mapper():
             word_count[word] = word_count.get(word, 0) + 1
         # 输出每个单词及其出现次数，key为单词，value为包含文件名和出现次数的字典
         for word, count in word_count.items():
-            print(f'{word}\t{file_name}:{count}')
+            print(f'{word}\t{file_name}\t{url}:{count}')
 
 
 if __name__ == '__main__':
+    #    read_file("new_pages/1.txt")
     mapper()
--- reducer.py
+++ reducer.py
@@ -3,19 +3,18 @@
 import sys
 import csv
 
-#记录每个单词在具体文件中的出现次数
+# 记录每个单词在具体文件中的出现次数
 word_doc_count = {}
 
-#记录每个文件中的单词总数
+# 记录每个文件中的单词总数
 doc_total_words = {}
 
 for line in sys.stdin:
-    # 获取输出结果中的单词和文件名
-    word, file_info = line.strip().split('\t')
-    # 获取文件名和单词在文件中的出现次数
-    doc_name, count = file_info.split(':')
+    doc_info, count = line.strip().rsplit(':', 1)
+    word, doc_name = doc_info.split('\t', 1)
     count = int(count)
-
+    # print(word)
+    # print(doc_name)
     # 统计每个单词在不同文件中的出现次数
     if word not in word_doc_count:
         word_doc_count[word] = {}
@@ -25,13 +24,14 @@ for line in sys.stdin:
     if doc_name not in doc_total_words:
         doc_total_words[doc_name] = 0
     doc_total_words[doc_name] += count
-    #计算每个单词在不同文件中的tf-idf值，并将结果输出到csv文件中
+    # 计算每个单词在不同文件中的tf-idf值，并将结果输出到csv文件中
     with open('tf_idf.csv', 'w', newline='', encoding='utf-8-sig') as f:
         writer = csv.writer(f)
-        writer.writerow(['word', 'doc_name', 'tf_idf'])
+        writer.writerow(['word', 'doc_name', 'doc_url', 'tf_idf'])
         for word, doc_count in word_doc_count.items():
             for doc_name, count in doc_count.items():
                 tf = count / doc_total_words[doc_name]
                 idf = len(doc_total_words) / len(doc_count.keys())
                 tf_idf = tf * idf
-                writer.writerow([word, doc_name, tf_idf])
+                doc_id, doc_url = doc_name.split('\t')
+                writer.writerow([word, doc_id, doc_url, tf_idf])
